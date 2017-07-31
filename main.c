b206e7655254a630251ac06350edfeb8eb117469
y0ubat <y0u_bat@naver.com>
2017-07-31 13:51:00 -0700

    broadcast packet fix

--- main.c
+++ main.c
@@ -76,7 +76,7 @@ void send_packet(char *interface,char *sip,char *dip)
     for(int i=0;i<6;i++)
         eth.dmac[i] = 0xff;
 
-    memcpy(eth.smac,get_mac("ens33"),6);
+    memcpy(eth.smac,get_mac(interface),6);
 
     eth.type = ntohs(0x0806);
 
@@ -84,19 +84,19 @@ void send_packet(char *interface,char *sip,char *dip)
 
     arp.htype = ntohs(0x0001);
     arp.ptype = ntohs(0x0800);
-    arp.hlen = "\x06";
-    arp.plen = "\x04";
+    arp.hlen = 6;
+    arp.plen = 4;
     arp.oper = ntohs(0x0001);
 
-    memcpy(arp.sha,get_mac("ens33"),6);
+    memcpy(arp.sha,get_mac(interface),6);
 
-    tmp = ntohsl(sip);
 
-    memcpy(arp.spa,sip,4);
-    tmp =0;
+
+    memcpy(arp.spa,&sip,4);
+
     memcpy(arp.tha,&eth.dmac,6);
-    tmp = ntohl(dip);
-    memcpy(arp.tpa,dip,4);
+
+    memcpy(arp.tpa,&dip,4);
 
     memcpy(all_packet+14,&arp,28);
 }
@@ -112,19 +112,18 @@ int main(int argc, char *argv[])
 
     int res;
 
-    /*  if(argc != 4)
+      if(argc != 4)
     {
         printf("[Using] ./program [interface] [sip] [dip]\n");
         exit(1);
-    }*/
+    }
 
 
 
-  //  inet_pton(AF_INET,"192.168.1.145",&addr.s_addr);
-    //inet_pton(AF_INET,"192.168.1.1",&addr2.s_addr);
-    send_packet("ens33",inet_addr("192.168.1.145"),inet_addr("192.168.1.1"));
 
-    handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
+    send_packet(argv[1],inet_addr(argv[2]),inet_addr(argv[3]));
+
+    handle = pcap_open_live(argv[1], BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
         //     printf("Couldn't open device %s: %s", interface, errbuf);
         exit(1);
